a74544a1c1a0631016babe2ec0869b251dc50ff1
AnhHaoGit <nguyenanhhao090708@gmail.com>
2026-08-03 20:14:11 +0700

    implement fixes

--- controllers/database/user.controller.ts
+++ controllers/database/user.controller.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import { connectToDatabase } from "../../libs/connect-db.ts";
 
 export async function getUserData(req: Request, res: Response) {
+  console.log("getUserData called");
   const userId = req.user?.sub;
   if (!userId) {
     return res.status(401).json({ error: "Authentication required" });
